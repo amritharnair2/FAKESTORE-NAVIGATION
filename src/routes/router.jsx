a238a600e6a9d98f2e4cfdaaf77da278016240b1
amritharnair2 <135534765+amritharnair2@users.noreply.github.com>
2025-02-08 20:28:41 +0530

    Update router.jsx

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,17 +1,14 @@
 import {createBrowserRouter} from "react-router-dom";
-import Aboutpage, { getProductDetails } from "../pages/Productdetails";
 import Userlayout from "../layouts/Userlayout";
 import Productlayout from "../layouts/Productlayout";
   
-
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <Userlayout/>
     },
     {
-      path: "productdetails/:id",
+      path: "Productdetails/:id",
       element: <Productlayout/>,
-      loader: getProductDetails
     }
   ]);
