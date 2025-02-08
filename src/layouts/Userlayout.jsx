31b39eed800ee468037e140d75858b3a67efc45f
amritharnair2 <135534765+amritharnair2@users.noreply.github.com>
2025-02-08 16:48:28 +0530

    Update Userlayout.jsx

--- src/layouts/Userlayout.jsx
+++ src/layouts/Userlayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Homepage from '../pages/HomePage'
+import Homepage from '../pages/Homepage'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
